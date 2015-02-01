c1487e8b7b0d5e9d9990d88a172db11fa249f835
horo <s0m1n2@nate.com>
2015-02-01 20:57:09 +0900

    2015-02-01 아직 작업중

---
+++ js/main.js
@@ -0,0 +1,29 @@
+(function () {
+
+    $(document).ready(function () {
+       // $('#portfolioModal').modal('show');
+    });
+}).call(this);
+
+
+function showModal(obj) {
+    var title = $(obj).children('.itemInfo').children('.item_title').children('.itemTitleString').text();
+    $('#projectTitle').text(title);
+    $('#portfolioModal').modal('show');
+    switch (title) {
+        case '핫스페이스' : 
+            break;
+        case '위패스' : 
+            break;            
+        case '오늘의특급호텔' : 
+            break;
+        case '위패스 어드민' : 
+            break;
+        case '위패스 점주앱' : 
+            break;
+        case '어이브라더' : 
+            break;
+        case '어이시스터' : 
+            break;            
+    }
+}
