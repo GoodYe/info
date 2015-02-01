44bdb4f57974ad1838034bef3f15affc68cff4f1
horo <s0m1n2@nate.com>
2015-02-01 22:49:28 +0900

    test

--- js/main.js
+++ js/main.js
@@ -10,11 +10,12 @@ function showModal(obj) {
     var title = $(obj).children('.itemInfo').children('.item_title').children('.itemTitleString').text();
     $('#projectTitle').text(title);
     //$('#portfolioModal').modal('show');
-    $.getJSON( "../path/portfolio.json", function( data ) {
-        console.log(data);
-    });    
-    
-    
+    var jqxhr = $.getJSON("../path/portfolio.json", function() {
+      alert("success");
+    })
+    .success(function() { alert("second success"); })
+    .error(function() { alert("error"); })
+    .complete(function() { alert("complete"); });    
     switch (title) {
         case '핫스페이스' : 
             break;
